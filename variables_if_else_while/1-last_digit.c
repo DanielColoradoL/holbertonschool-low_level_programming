ccbeb534130cb40184d8eaea8d0554411a7adfaa
Daniel Colorado L <danielcoloradol@hotmail.com>
2023-10-16 22:24:26 -0700

    1 third

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,28 +4,30 @@
 
 /**
  * main - Entry point
- * Description: 'Checks last digit of a number and pass it trough and if'
+ * Description: 'Checks last digit of a number
+ *and pass it trough and if'
  * Return: Always 0.
  */
 int main(void)
 {
 	int n;
 	int last_digit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
 	if (last_digit > 5)
 	{
-		printf("Last digit of %i and is greater than 5\n", n);
+		printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
 	}
 	else if (last_digit == 0)
 	{
-		printf("Last digit of %i and is 0\n", n);
+		printf("Last digit of %i is %i and is 0\n", n, last_digit);
 	}
 	else if (last_digit < 6 && last_digit != 0)
 	{
-		printf("Last digit of %i and is less than 6 and not 0\n", n);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
 	}
 
 	return (0);
