63de0cdbdaea2e02c58cdabf4c9cee1acedc30e9
Daniel <danielcoloradol@hotmail.com>
2023-11-21 18:16:51 -0800

    Update to definitions

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,9 +18,8 @@ struct dog
 };
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for Struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
